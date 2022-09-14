4bf02755dff3de657a152cd7b3a424a2c3c50ca0
BarryDigby <b.digby237@gmail.com>
2022-09-14 13:10:34 -0700

    reformat_vcf refactor

--- bin/reformat_vcf.py
+++ bin/reformat_vcf.py
@@ -1,19 +1,16 @@
 from pysam import VariantFile
 import os
 
-# AUTHOR: @gudeqing
+# Inspired by: @gudeqing
 # https://github.com/sigven/pcgr/issues/136#issuecomment-919273152
+# Built upon by @BarryDigby to handle Strelka, Freebayes, Mutect2 VCF files.
 
-def reformat_vcf(vcf_file, out, reference, tumor_sample=None):
+def reformat_vcf(vcf_file, out, reference):
     """
-    [split and left-normalization] -> [re-calculate AF] -> [bgzip and tabix]
-    :param vcf_file:
-    :param out:
-    :param reference:
-    :param tumor_sample: tumor sample name or position, if the second sample is tumor, then it is 1 else 0
-    :return:
+    BCFtools: split multi-allelic sites, normalize and remove sites where DP is missing or < 1. (divide by zero errors).
+    Calculations need to be cross checked with someone with more experience in population genomics.
     """
-    os.system(f'bcftools norm -f {reference} -m -both {vcf_file} -o tmp_.vcf')
+    os.system(f'bcftools norm -f {reference} -m -both {vcf_file} | bcftools filter -e\'FORMAT/DP="." || FORMAT/DP<1\' -o tmp_.vcf')
     with VariantFile('tmp_.vcf') as fr:
         header = fr.header
         header.info.add('TDP', number=1, type='Integer', description='Tumor sample depth')
@@ -21,54 +18,76 @@ def reformat_vcf(vcf_file, out, reference, tumor_sample=None):
         header.info.add('TAF', number=1, type='Float', description='Tumor sample AF')
         header.info.add('NAF', number=1, type='Float', description='Normal sample AF')
         samples = list(header.samples)
-
-        if tumor_sample is not None:
-            if tumor_sample not in [0, 1, '0', '1']:
-                if tumor_sample in samples:
-                    tumor_idx = samples.index(tumor_sample)
-                    normal_idx = 1 - tumor_idx
-                else:
-                    raise Exception(f'{tumor_sample} is not in samples {samples} recorded in vcf')
-            else:
-                tumor_idx = int(tumor_sample)
-                normal_idx = 1 - tumor_idx
-        else:
-            tumor_idx = guess_tumor_idx(vcf_file)
-            normal_idx = 1 - tumor_idx
-
+        formats = list(header.formats)
+        fnc_str = list(vcf_formats.keys())[list(vcf_formats.values()).index(list(formats))]
+        if 'strelka' in fnc_str:
+            header.formats.add('AD', number=2, type="Integer", description='AD flag for Strelka. Output as tuple so index rule for TAF does not need to be modified.')
         with VariantFile(out, 'w', header=header) as fw:
             for record in fr:
+                VAF_sample0 = locals()[fnc_str](record, 0)
+                VAF_sample1 = locals()[fnc_str](record, 1)
+                if VAF_sample0 > VAF_sample1:
+                    tumor_is_first += 1
+                else:
+                    tumor_is_second += 1
+                tumor_idx = tumor_is_second >= tumor_is_first
+                normal_idx = 1 - tumor_idx
                 record.info['TDP'] = record.samples[tumor_idx]['DP']
                 record.info['NDP'] = record.samples[normal_idx]['DP']
-                # re-calculate AF since caller like sentieon may report AF that is not consistent with AD info
                 record.info['TAF'] = round(record.samples[tumor_idx]['AD'][1]/record.samples[tumor_idx]['DP'], 3)
                 record.info['NAF'] = round(record.samples[normal_idx]['AD'][1]/record.samples[normal_idx]['DP'], 3)
+                print(record)
                 fw.write(record)
 
     os.remove('tmp_.vcf')
     os.system(f'bgzip {out}')
     os.system(f'tabix {out}.gz')
 
-
-def guess_tumor_idx(vcf_file):
-    tumor_is_first = 0
-    tumor_is_second = 0
-
-    with VariantFile(vcf_file) as fr:
-        samples = list(fr.header.samples)
-        formats = list(fr.header.formats)
-        if 'AF' not in formats:
-            raise Exception('No AF in format info to detect tumor sample')
-        for record in fr:
-            if record.samples[0]['AF'][0] > record.samples[1]['AF'][0]:
-                tumor_is_first += 1
-            else:
-                tumor_is_second += 1
-    tumor_idx = tumor_is_second >= tumor_is_first
-    print(f'we guess tumor sample is {samples[tumor_idx]} ')
-    return tumor_idx
+def mutect2_vaf(record, sample_idx):
+    VAF = record.samples[sample_idx]['AF'][0]
+    VAF = VAF if VAF is not None else 0
+    return VAF
 
 
-if __name__ == '__main__':
-    from xcmds import xcmds
-    xcmds.xcmds(locals())
+def freebayes_vaf(record, sample_idx):
+    AO = record.samples[sample_idx]['AO'][0]
+    RO = record.samples[sample_idx]['RO']
+    AO = AO if AO is not None else 0
+    RO = RO if RO is not None else 0
+    if (AO+RO) == 0:
+        VAF = 0
+    else:
+        VAF = (AO/(AO+RO))
+    return VAF
+
+def strelka_snv_vaf(record, sample_idx):
+    ref = str(record.ref + "U")
+    alt = str(record.alts[0] + "U")
+    tier1RefCounts = record.samples[sample_idx][ref][0]
+    tier1AltCounts = record.samples[sample_idx][alt][0]
+    tier1RefCounts = tier1RefCounts if tier1RefCounts is not None else 0
+    tier1AltCounts = tier1AltCounts if tier1AltCounts is not None else 0
+    if (tier1AltCounts+tier1RefCounts) == 0:
+        VAF = 0
+    else:
+        VAF = (tier1AltCounts/(tier1AltCounts+tier1RefCounts))
+    record.samples[sample_idx]['AD'] = [0, tier1AltCounts]
+    return VAF
+
+def strelka_indel_vaf(record, sample_idx):
+    tier1RefCounts = record.samples[sample_idx]['TAR'][0]
+    tier1AltCounts = record.samples[sample_idx]['TIR'][0]
+    tier1RefCounts = tier1RefCounts if tier1RefCounts is not None else 0
+    tier1AltCounts = tier1AltCounts if tier1AltCounts is not None else 0
+    if (tier1AltCounts+tier1RefCounts) == 0:
+        VAF = 0
+    else:
+        VAF = (tier1AltCounts/(tier1AltCounts+tier1RefCounts))
+    record.samples[sample_idx]['AD'] = [0, tier1AltCounts]
+    return VAF
+
+
+vcf_formats = { "mutect2_vaf": ['AD', 'AF', 'DP', 'F1R2', 'F2R1', 'FAD', 'GQ', 'GT', 'PGT', 'PID', 'PL', 'PS', 'SB'],
+                "freebayes_vaf": ['AD', 'AO', 'DP', 'GL', 'GQ', 'GT', 'MIN_DP', 'PL', 'QA', 'QR', 'RO'],
+                "strelka_snv_vaf": ['AU', 'CU', 'DP', 'FDP', 'GU', 'SDP', 'SUBDP', 'TU'],
+                "strelka_indel_vaf": ['BCN50', 'DP', 'DP2', 'DP50', 'FDP50', 'SUBDP50', 'TAR', 'TIR', 'TOR'] }
